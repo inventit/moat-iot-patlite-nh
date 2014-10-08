23cc4385e2abffc9ccbbdf63c3f41503ddf0a1e9
Daisuke Baba <dbaba@yourinventit.com>
2014-10-08 17:26:57 +0900

    Adds a new function ok_got_it()

--- tools/patlite-nh.py
+++ tools/patlite-nh.py
@@ -2,11 +2,14 @@
 
 import socket
 import binascii
+import time
 
 TCP_IP = '192.168.1.245'
 TCP_PORT = 10000
 BUFFER_SIZE = 1024
 HEADER = "\x58\x58\x53\x00\x00\x06"
+HEADER_OK_GOT_IT = "\x58\x58\x43\x00\x00\x00"
+HEADER_TELL_ME = "\x58\x58\x47\x00\x00\x00"
 
 LIGHT = {
     "OFF" :       "\x00",
@@ -25,7 +28,7 @@ BUZZER = {
     "NOT_CHANGE": "\x09"
 }
 
-class RequestBuilder:
+class NHRequestBuilder:
     def __init__(self):
         self.params = {}
         self._light("red", None)
@@ -34,6 +37,7 @@ class RequestBuilder:
         self._light("blue", None)
         self._light("white", None)
         self.buzzer(None)
+        self.params["ok_got_it"] = False
     
     def _light(self, light, arg):
         try:
@@ -41,15 +45,19 @@ class RequestBuilder:
         except KeyError:
             val = LIGHT["NOT_CHANGE"]
         self.params[light] = val
+        self.params["ok_got_it"] = False
     
     def build(self):
-        ret = HEADER
-        ret += self.params["red"]
-        ret += self.params["yellow"]
-        ret += self.params["green"]
-        ret += self.params["blue"]
-        ret += self.params["white"]
-        ret += self.params["buzzer"]
+        if self.params["ok_got_it"]:
+            ret = HEADER_OK_GOT_IT
+        else:
+            ret = HEADER
+            ret += self.params["red"]
+            ret += self.params["yellow"]
+            ret += self.params["green"]
+            ret += self.params["blue"]
+            ret += self.params["white"]
+            ret += self.params["buzzer"]
         return ret
 
     def buzzer(self, arg):
@@ -58,6 +66,7 @@ class RequestBuilder:
         except KeyError:
             val = BUZZER["NOT_CHANGE"]
         self.params["buzzer"] = val
+        self.params["ok_got_it"] = False
         return self
         
     def red(self, arg):
@@ -76,7 +85,11 @@ class RequestBuilder:
         self._light("white", arg)
         return self
 
-req = RequestBuilder().red("BLINK2").yellow("BLINK1").green("BLINK2").buzzer("SIREN2").build()
+    def got_it(self):
+        self.params["ok_got_it"] = True
+        return self
+
+req = NHRequestBuilder().red("BLINK2").yellow("BLINK1").green("BLINK1").build()
 print binascii.hexlify(req)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print"0>> connecting"
@@ -85,5 +98,15 @@ print"1>> sending"
 s.send(req)
 print"2>> receiving"
 res = s.recv(BUFFER_SIZE)
-s.close()
 print binascii.hexlify(res)
+
+time.sleep(5)
+
+req = NHRequestBuilder().got_it().build()
+print"1>> sending"
+s.send(req)
+print"2>> receiving"
+res = s.recv(BUFFER_SIZE)
+print binascii.hexlify(res)
+
+s.close()
